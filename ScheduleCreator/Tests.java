6d2c3ad41c6273b71174bbc4b308bc5de66ae81c
Nick Econopouly <wry@mm.st>
2020-04-20 19:10:35 -0400

    Remove outdated semester test from Tests.java

--- ScheduleCreator/Tests.java
+++ ScheduleCreator/Tests.java
@@ -19,11 +19,11 @@ public class Tests {
 
     public static void main(String[] args) throws IOException, MailjetException, MailjetSocketTimeoutException {
 
-        //regen databse
-        Admin.regenDB();
+        // uncomment and run this once when we add a new semester
+        // Admin.regenDB();
 
         //test validate method
-        //emailMethodTestData();
+        emailMethodTestData();
 
     }
 
@@ -66,40 +66,6 @@ public class Tests {
             System.out.println("FAILED : \"" + _email + "\" Is NOT a valid email. EXPECTED: " + _expectedResults);
         }
         return null;
-
-    }
-
-    public static void testSemester() throws IOException {
-
-        // Example usage of DBAdapter
-        List<String> semesters = new Translator().getSemesters();
-
-        System.out.println("Current Semesters are:");
-        for (int i = 0; i < semesters.size(); i++) {
-            System.out.println(semesters.get(i));
-        }
-
-        // choose a semester
-        String semester = semesters.get(0);
-
-        // get courses
-        List<String> courses = Tests.adapter.getCourses(semester);
-
-        // example course from the semester
-        String exampleCourse = courses.get(20);
-
-        System.out.println("Example course is: " + exampleCourse);
-
-        // dummy method - we still need to implement this (I think?)
-        List<String> sections = new Translator().getSections(exampleCourse, semester);
-        String section = sections.get(0);
-
-        //should return real info for CSC 250 - 01
-        System.out.println("Building for " + section + " is: ");
-        System.out.println(new Translator().getSectionInfo(Translator.choice.BUILDING, semester, section));
-
-        System.out.println("CRN for " + section + " is: ");
-        System.out.println(new Translator().getSectionInfo(Translator.choice.CRN, semester, section));
 
     }
 }
